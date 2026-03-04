874e0c99faac47dfe19b7bab2dad4fcb44fbd096
Miky-dev <miky.vitella@gmail.com>
2026-03-04 22:20:25 +0100

    eliminazione test

--- JarvisLocale/test.py
+++
@@ -1,7 +0,0 @@
-﻿print('step 1')
-from tools_calendar import ottieni_eventi_precaricati
-print('step 2')
-from tools_routine import imposta_sveglia
-print('step 3')
-import dateparser
-print('step 4')
